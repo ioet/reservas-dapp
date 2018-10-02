892539efdaa8c818cc05d0d00e307563774050b0
Janina Tatiana Herrera Riofrio <jherrera@ioet.com>
2018-10-01 21:00:49 -0500

    Add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header-component';
+
+import { AppRoutingModule } from './app-routing.module';
+
 import {MetaModule} from "./meta/meta.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+	HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+	AppRoutingModule,
     MetaModule
   ],
   providers: [],
